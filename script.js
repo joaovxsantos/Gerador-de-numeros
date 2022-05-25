811de87eb303ac370d9b1e4a03d4efe7abf70b42
João Vitor <joaovitor55670@gmail.com>
2022-05-25 20:20:42 -0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+// function number(){
+//     let a = document.getElementById("valor")
+//     let valor = a.value
+//     let b = window.document.getElementsByTagName("p")[1]
+//     let c = Math.floor(Math.random() * valor + 1)
+//     b.innerText = "Número gerado: " + c
+
+// }
+
+
+// const getRandomNumber = (min, max) => {
+//     console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//   };
+
+let a = document.getElementById("min");
+let min = a.value
+let b = document.getElementById("max");
+let max = b.value
+let c = window.document.getElementsByTagName("p")[1];
+console.log(min)
+
 function number(){
-    let a = document.getElementById("valor")
-    let valor = a.value
-    let b = window.document.getElementsByTagName("p")[1]
-    let c = Math.floor(Math.random() * valor + 1)
-    b.innerText = "Número gerado: " + c
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+    c.innerText = "Número gerado: " + result;
+    
 
 }
+
+
+
